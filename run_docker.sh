3b5c22351a4865c363748636c12ace6f87477955
Arjen Markus <arjen.markus@deltares.nl>
2018-06-14 12:39:09 +0200

    Updated the run_docker script to make sure DELWAQ is called correctly
    More specifically:
    - The hydrodynamic files shoud be stored in /data-flowout (implicit
      in the input files - not visible in the run procedure)
    - The input files for DELWAQ are supposed to be located in /data-waqin
    - The result files are copied to /data-waqout
    - The procedure expects the file "runid" to contain the name of the "master"
      input file.
    
    To allow the user to examine the run, a log file "delwaq.log" is used that
    captures everything written to standard output/error.

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 #
-# Set the config file here
+# Check that a file "runid" exists ...
 #
-# argfile=config_d_hydro.xml
+if [ ! -f /data-waqin/runid ]; then
+    echo Error: no run-ID file given > delwaq.log
+    exit
+fi
 
 #
-# Set the directory containing delftflow.exe here
+# Set the directory containing delwaq executables here
 #
 export ARCH=lnx64
 export D3D_HOME=/opt/delft3d_4.03.00
@@ -19,12 +22,22 @@ exedir=$D3D_HOME/$ARCH/waq/bin
 # Set some (environment) parameters
 export LD_LIBRARY_PATH=$exedir:$LD_LIBRARY_PATH
 
-# Run 1
-ls -l /opt
-$exedir/delwaq1 /data-waqin/params.inp
-ls .
+# Run DELWAQ1
 
-$exedir/delwaq2 /data-waqin/params.inp
-ls .
+cp -r /data-waqin/* .
+echo y >> runid
 
-cp params.* /data-waqout
+$exedir/delwaq1 -p $exedir/../default/proc_def <runid 1>delwaq.log 2>&1
+rc=$?
+
+# Run DELWAQ2 if possible
+
+if [ $rc -eq 0 ]; then
+    $exedir/delwaq2 <runid 1>>delwaq.log 2>&1
+else
+    echo !                                        >>delwaq.log
+    echo ! Error running DELWAQ1 - please check!  >>delwaq.log
+    echo !                                        >>delwaq.log
+fi
+
+cp * /data-waqout
